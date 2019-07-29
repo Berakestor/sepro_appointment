be0df52feef4a68f219a8af1c3c279e949bd8ff1
Lukas Laaser <lukas.laaser@web.de>
2019-07-29 14:40:26 +0200

    Initialize project with Spring

---
+++ src/main/java/de/sepro/termin/TerminApplication.java
@@ -0,0 +1,13 @@
+package de.sepro.termin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/sepro/termin/TerminApplicationTests.java
@@ -0,0 +1,16 @@
+package de.sepro.termin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TerminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
